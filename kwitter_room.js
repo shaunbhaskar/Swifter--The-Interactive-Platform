3c1d6e2c8b937d349a21e892db9ec8e0314430fe
shaunbhaskar <73226217+shaunbhaskar@users.noreply.github.com>
2021-08-02 12:18:24 +0530

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,13 +1,13 @@
 // Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyA-O8dgTT-nb-X9ed0YumLH4BOyHSxogm4",
-  authDomain: "swifter-66672.firebaseapp.com",
-  databaseURL: "https://swifter-66672-default-rtdb.firebaseio.com",
-  projectId: "swifter-66672",
-  storageBucket: "swifter-66672.appspot.com",
-  messagingSenderId: "56010265469",
-  appId: "1:56010265469:web:44d9c654516104bd443214"
-};
+ var firebaseConfig = {
+    apiKey: "AIzaSyA-O8dgTT-nb-X9ed0YumLH4BOyHSxogm4",
+    authDomain: "swifter-66672.firebaseapp.com",
+    databaseURL: "https://swifter-66672-default-rtdb.firebaseio.com",
+    projectId: "swifter-66672",
+    storageBucket: "swifter-66672.appspot.com",
+    messagingSenderId: "56010265469",
+    appId: "1:56010265469:web:44d9c654516104bd443214"
+  };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
     user_name = localStorage.getItem("user_name");
