7cf61eb51cf492d462a1a9346bab353a495f4df7
shaunbhaskar <73226217+shaunbhaskar@users.noreply.github.com>
2021-08-02 12:21:49 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,12 +1,12 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyA-O8dgTT-nb-X9ed0YumLH4BOyHSxogm4",
-  authDomain: "swifter-66672.firebaseapp.com",
-  databaseURL: "https://swifter-66672-default-rtdb.firebaseio.com",
-  projectId: "swifter-66672",
-  storageBucket: "swifter-66672.appspot.com",
-  messagingSenderId: "56010265469",
-  appId: "1:56010265469:web:44d9c654516104bd443214"
-};
+ var firebaseConfig = {
+    apiKey: "AIzaSyA-O8dgTT-nb-X9ed0YumLH4BOyHSxogm4",
+    authDomain: "swifter-66672.firebaseapp.com",
+    databaseURL: "https://swifter-66672-default-rtdb.firebaseio.com",
+    projectId: "swifter-66672",
+    storageBucket: "swifter-66672.appspot.com",
+    messagingSenderId: "56010265469",
+    appId: "1:56010265469:web:44d9c654516104bd443214"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
   
